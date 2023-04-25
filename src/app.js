439e0986633884140815b8367ece73249ed60374
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-04-25 17:15:49 -0300

    validation and signin added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import cors from "cors"
-import { MongoClient, ObjectId } from "mongodb"
-import dotenv from "dotenv"
-import joi from "joi"
+import bcrypt from 'bcrypt';
+import { userSchema } from "./schema/user.schema.js"
+import { userCollection } from './database/db.js'
+import { v4 as uuid } from 'uuid';
 
+const token = uuid();
 
 
 // Server 
@@ -12,63 +14,42 @@ const app = express()
 // Setup
 app.use(express.json())
 app.use(cors())
-dotenv.config()
-
-
-// Database Setup
-const mongoClient = new MongoClient(process.env.DATABASE_URL)
-let db;
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message))
 
 
 
-// JOI SCHEMA
-const userSchema = joi.object({
-    name: joi.string().required(),
-    email: joi.string().email().required(),
-    password: joi.string().min(3).required(),
-    confirmPassword: joi.ref("password")
-  })
-
-
-
-// validate User Schema
 
 // Endpoints
 app.post("/cadastro", async (req, res) => {
-    const { name, email, password, confirmPassword } = req.body
-  
-    // Validate User Schema
-    const { error } = userSchema.validate({ name, email, password, confirmPassword }, { abortEarly: false })
-  
+    const { name, email, password } = req.body
+
+    const { error } = userSchema.validate({ name, email, password }, { abortEarly: false })
+
     if (error) {
       const errors = error.details.map((detail) => detail.message);
       return res.status(422).send(errors);
     }
-  
-    
-    const existingUser = await db.collection("user").findOne({ email })
+
+    const existingUser = await userCollection.findOne({ email })
+
     if (existingUser) {
       return res.status(409).send("User with this email already exists")
     }
-  
-    
+
     const hashedPassword = await bcrypt.hash(password, 10)
-  
-   
-    const result = await db.collection("user").insertOne({
-      name,
-      email,
-      password: hashedPassword,
-      createdAt: new Date(),
+
+    try {
+        const result = await userCollection.insertOne({
+          name,
+          email,
+          password: hashedPassword,
+        })
+        res.status(201)
+      } catch (error) {
+        console.error(error)
+        res.status(500).send("Internal Server Error")
+      }
+
     })
-  
-    res.status(201).send(result.ops[0])
-  })
-
-
 // Port Server
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+
+try {
+  await mongoClient.connect()
+  console.log('Conectou com o mongoDB')
+} catch (error) {
+  console.error(error)
+}
+
+const db = mongoClient.db()
+
+export const userCollection = db.collection('user')
