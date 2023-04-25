e8012f2c8c2b56083a6878dbc29a54e117050299
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-04-25 15:21:28 -0300

    validation and JOI schema for useres added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import express from "express"
-import express from "express"
 import cors from "cors"
 import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
-import express from "express"
 
 
 
@@ -24,6 +22,51 @@ mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message))
 
+
+
+// JOI SCHEMA
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(3).required(),
+    confirmPassword: joi.ref("password")
+  })
+
+
+
+// validate User Schema
+
+// Endpoints
+app.post("/cadastro", async (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+  
+    // Validate User Schema
+    const { error } = userSchema.validate({ name, email, password, confirmPassword }, { abortEarly: false })
+  
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      return res.status(422).send(errors);
+    }
+  
+    
+    const existingUser = await db.collection("user").findOne({ email })
+    if (existingUser) {
+      return res.status(409).send("User with this email already exists")
+    }
+  
+    
+    const hashedPassword = await bcrypt.hash(password, 10)
+  
+   
+    const result = await db.collection("user").insertOne({
+      name,
+      email,
+      password: hashedPassword,
+      createdAt: new Date(),
+    })
+  
+    res.status(201).send(result.ops[0])
+  })
 
 
 // Port Server
